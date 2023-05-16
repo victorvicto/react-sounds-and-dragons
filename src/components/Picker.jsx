a69aaf9c03e32cc3cd314ce1695c86aa6f7328b6
victorvicto <victor.demu@gmail.com>
2023-05-16 23:46:42 +0200

    Fixed pickers

--- src/components/Picker.jsx
+++ src/components/Picker.jsx
@@ -1,26 +1,24 @@
 import { useState } from 'react'
 
 function Picker({name, possibilities}) {
+    console.log(possibilities[0]);
     const [active_radio, set_active_radio] = useState(possibilities[0]);
 
     function radio_click(name){
+        console.log(name);
         set_active_radio(name);
     }
 
     const radio_buts = []
-    var i = 0
     for(var p of possibilities){
-        radio_buts.push(<input type='radio' className='btn-check' id={p+"-btnradio"} key={p+"-btnradio"} value={p} onClick={()=>{radio_click(p)}} active={(active_radio==p).toString()} autoComplete='off'/>);
-        radio_buts.push(<label className='btn btn-outline-primary mt-1' htmlFor={p+"-btnradio"} key={p+"-btnradio-label"}>{p}</label>);
-        i++;
+        radio_buts.push(<input type='radio' className='btn-check' id={name+"-"+p+"-btnradio"} key={name+"-"+p+"-btnradio"} value={p} onClick={(e)=>{radio_click(e.target.value);}} checked={active_radio==p} autoComplete='off'/>);
+        radio_buts.push(<label className='btn btn-outline-primary mt-1' htmlFor={name+"-"+p+"-btnradio"} key={name+"-"+p+"-btnradio-label"}>{p}</label>);
     }
   
     return (
         <div className='card m-2'>
-            <div className='card-title'>
-                <h2 className='card-title'>{ name }</h2>
-            </div>
             <div className='card-body'>
+                <h2 className='card-title'>{ name }</h2>
                 <div className='btn-group d-flex flex-wrap' role='group'>
                     {radio_buts}
                 </div>
