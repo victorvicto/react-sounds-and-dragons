b182bc81535bcddb4773a00a70d8e2dcda5a18fb
DEMUYSERE Victor <victor.demu@gmail.com>
2023-06-07 11:56:35 +0200

    Finished modifiers filler

--- src/components/PlayScreen.jsx
+++ src/components/PlayScreen.jsx
@@ -10,9 +10,34 @@ function PlayScreen() {
 
     const regions = Object.keys(sound_lore["regions"]);
     const places = Object.keys(sound_lore["places"]);
-    var music_contexts = Object.keys(sound_lore["music contexts"]);
-    music_contexts = music_contexts.concat(Object.keys(sound_lore["places"][active_place]["music contexts"]));
-    const modifiers = Object.keys(sound_lore["places"]);
+    var music_contexts = new Set();
+    for (const music_con of Object.keys(sound_lore["music contexts"])){
+        music_contexts.add(music_con);
+    }
+    for (const music_con of Object.keys(sound_lore["places"][active_place]["music contexts"])){
+        music_contexts.add(music_con);
+    }
+    var modifiers = new Set();
+    modifiers.add("no modifier");
+    if ("ambiance modifiers" in sound_lore["places"][active_place]){
+        for (const mod of Object.keys(sound_lore["places"][active_place]["ambiance modifiers"])){
+            modifiers.add(mod);
+        }
+    }
+    if ("theme modifiers" in sound_lore["music contexts"][active_music_context]){
+        for (const mod of Object.keys(sound_lore["music contexts"][active_music_context]["theme modifiers"])){
+            modifiers.add(mod);
+        }
+    }
+    if ("music contexts" in sound_lore["places"][active_place]){
+        if (active_music_context in sound_lore["places"][active_place]["music contexts"]){
+            if ("theme modifiers" in sound_lore["places"][active_place]["music contexts"][active_music_context]){
+                for (const mod of Object.keys(sound_lore["places"][active_place]["music contexts"][active_music_context]["theme modifiers"])){
+                    modifiers.add(mod);
+                }
+            }
+        }
+    }
   
     return (
         <>
@@ -30,7 +55,7 @@ function PlayScreen() {
                     active_radio={active_music_context}
                     set_active_radio={set_active_music_context}/>
             <Picker name="Modifier" 
-                    possibilities={regions} 
+                    possibilities={modifiers} 
                     active_radio={active_modifier}
                     set_active_radio={set_active_modifier}/>
         </>
