487e7dcabf6e5d5ceef640b0232bbd653651a339
DEMUYSERE Victor <victor.demu@gmail.com>
2023-06-15 13:26:22 +0200

    trying to solve key error

--- src/components/Picker.jsx
+++ src/components/Picker.jsx
@@ -7,7 +7,7 @@ function Picker({name, possibilities, active_radio, set_active_radio, disabled})
         set_active_radio(name);
     }
 
-    const radio_buts = [];
+    var radio_buts = [];
     for(var p of possibilities){
         // radio_buts.push(<input  type='radio' 
         //                         className='btn-check' 
@@ -25,7 +25,8 @@ function Picker({name, possibilities, active_radio, set_active_radio, disabled})
                                         group_name={name}
                                         selected={p==active_radio}
                                         disabled={disabled}
-                                        onclick={(e)=>{radio_click(e.target.value);}}/>)
+                                        onclick={(e)=>{radio_click(e.target.value);}}
+                                        key={name+"-"+p+"-radiobutton"}/>)
     }
   
     return (
@@ -33,7 +34,14 @@ function Picker({name, possibilities, active_radio, set_active_radio, disabled})
             <div className='card-body'>
                 <h4 className='card-title'>{ name }</h4>
                 <div className='btn-group d-flex flex-wrap' role='group'>
-                    {radio_buts}
+                    {possibilities.map(p =>
+                        <RadioButton    title={p}
+                                        group_name={name}
+                                        selected={p==active_radio}
+                                        disabled={disabled}
+                                        onclick={(e)=>{radio_click(e.target.value);}}
+                                        key={name+"-"+p+"-radiobutton"}/>
+                    )}
                 </div>
             </div>
         </div>
