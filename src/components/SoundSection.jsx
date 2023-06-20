fadfe4cc7973087152cbd86a4ee007d9de37f301
DEMUYSERE Victor <victor.demu@gmail.com>
2023-06-20 11:58:29 +0200

    started sound page

--- src/components/SoundSection.jsx
+++ src/components/SoundSection.jsx
@@ -1,11 +1,22 @@
-function SoundsSection({sound_name}) {
+import { useState } from 'react';
 
-  
+function SoundSection({sound_name, files, category}) {
+    const [collapsed, set_collapsed] = useState(true);
+
     return (
-        <>
-            <h1>{sound_name}</h1>
-        </>
+        <div className='card m-2 shadow'>
+            <h4 className="card-header text-capitalize"
+                role="button"
+                onClick={() => {set_collapsed(!collapsed)}}>
+                    {sound_name+"   "}
+                    <i className={'small bi bi-chevron-'+(collapsed?"down":"up")}></i>
+            </h4>
+            <div className={"card-body collapse"+(collapsed?"":".show")}>
+                {category}
+            </div>
+        </div>
+
     )
 }
   
-export default SoundsSection
+export default SoundSection
--- src/components/SoundsScreen.jsx
+++ src/components/SoundsScreen.jsx
@@ -1,14 +1,24 @@
+import SoundSection from './SoundSection';
 import sound_lore from '../assets/sound_lore.json';
 
 function SoundsScreen() {
 
     var sound_sections = []
-
+    for(const [key, sound_info] of Object.entries(sound_lore["sounds"])){
+        sound_sections.push(
+            <SoundSection   key={key+'-sound'}
+                            sound_name={key}
+                            files={sound_info["files"]}
+                            category={sound_info["quicksound category"]}/>
+        )
+    }
   
     return (
         <>
             <h1>Sounds</h1>
-
+            <div>
+                {sound_sections}
+            </div>
         </>
     )
   }
