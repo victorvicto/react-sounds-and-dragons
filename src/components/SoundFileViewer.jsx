6ec7296f4619f2e5a039f1028579545e34fac7f1
DEMUYSERE Victor <victor.demu@gmail.com>
2023-06-21 15:51:35 +0200

    Sound viewer visuel termine

--- src/components/SoundFileViewer.jsx
+++ src/components/SoundFileViewer.jsx
@@ -4,18 +4,40 @@ import VolumeProfile from "./VolumeProfile.jsx";
 function SoundFileViewer({file}){
     const [start_time, set_start_time] = useState(file["start"]);
     const [end_time, set_end_time] = useState(file["end"]);
+    const [real_start_time, set_real_start_time] = useState(file["real start"]);
+    const [real_end_time, set_real_end_time] = useState(file["real end"]);
 
     function start_time_update(event){
-        if (event.target.value>=end_time){
-            set_start_time(end_time);
+        if (event.target.value>=real_start_time){
+            set_start_time(real_start_time);
         } else {
             set_start_time(event.target.value);
         }
     }
 
-    function end_time_update(event){
+    function real_start_time_update(event){
         if (event.target.value<=start_time){
-            set_end_time(start_time);
+            set_real_start_time(start_time);
+        } else if (event.target.value>=real_end_time) {
+            set_real_start_time(real_end_time);
+        } else {
+            set_real_start_time(event.target.value);
+        }
+    }
+
+    function real_end_time_update(event){
+        if (event.target.value<=real_start_time){
+            set_real_end_time(real_start_time);
+        } else if (event.target.value>=end_time) {
+            set_real_end_time(end_time);
+        } else {
+            set_real_end_time(event.target.value);
+        }
+    }
+
+    function end_time_update(event){
+        if (event.target.value<=real_end_time){
+            set_end_time(real_end_time);
         } else {
             set_end_time(event.target.value);
         }
@@ -26,24 +48,43 @@ function SoundFileViewer({file}){
     return (
         <li className="list-group-item">
             <div className="row align-items-center">
-                <div className="col-3">
-                    <div className="card">
+                <div className="col-4">
+                    <div className="card m-1">
                         <div className="card-body py-2">
                             <span className="opacity-25">{first_path_part+"/"}</span>
                             <b>{file_path_parts.slice(-1)}</b>
                         </div>
                     </div>
+                    <div className='m-1 row'>
+                        <div className='col-6 p-1 d-grid'>
+                            <button className='btn btn-outline-info'>Play</button>
+                        </div>
+                        <div className='col-6 p-1 d-grid'>
+                            <button className='btn btn-outline-success'>Save</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-7">
+                <div className="col-8">
                     <div className="card">
                         <div className="card-body py-2">
-                            <div className="row">
+                        <div className="row">
                                 <input type="range" min="0.0" max="1.0" step="0.001" value={start_time} onChange={start_time_update}/>
+                            </div>
+                            <div className="row">
+                                <input type="range" min="0.0" max="1.0" step="0.001" value={real_start_time} onChange={real_start_time_update}/>
+                            </div>
+                            {("volume profile" in file) && 
+                                <VolumeProfile  samples={file["volume profile"]} 
+                                                start={start_time} 
+                                                real_start={real_start_time} 
+                                                real_end={real_end_time}
+                                                end={end_time}/>}
+                            <div className="row">
+                                <input type="range" min="0.0" max="1.0" step="0.001" value={real_end_time} onChange={real_end_time_update}/>
                             </div>
                             <div className="row">
                                 <input type="range" min="0.0" max="1.0" step="0.001" value={end_time} onChange={end_time_update}/>
                             </div>
-                            {("volume profile" in file) && <VolumeProfile samples={file["volume profile"]} start={start_time} end={end_time}/>}
                         </div>
                     </div>
                 </div>
--- src/components/VolumeProfile.jsx
+++ src/components/VolumeProfile.jsx
@@ -1,9 +1,16 @@
-function VolumeProfile({samples, start, end}){
+function VolumeProfile({samples, start, real_start, real_end, end}){
     
     return (
         <div className="card volume-profile">
             {samples.map((val, i)=>{
-                return <div key={"sound-bar-"+i} className="sound-bar" style={{"height": val*100+"%"}}></div>
+                var mul = 1;
+                if (real_start*100>i) {
+                    mul = (i-start*100)/((real_start-start)*100);
+                } else if (real_end*100<i){
+                    mul = 1-((i-real_end*100)/((end-real_end)*100));
+                }
+                mul = Math.max(0,mul);
+                return <div key={"sound-bar-"+i} className="sound-bar" style={{"height": val*100*mul+"%"}}></div>
             })}
         </div>
     )
